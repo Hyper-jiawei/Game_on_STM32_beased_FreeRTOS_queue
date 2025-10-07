f3150c5d9541fece0e79cebf7d333aee900e89ec
Hyper-jiawei <jiaweibai1201@outlook.com>
2025-10-07 16:20:27 +0800

    initized

---
+++ Queue_game/MDK-ARM/01_freertos_template/resources.d
@@ -0,0 +1,6 @@
+01_freertos_template\resources.o: ..\nwatch\resources.c
+01_freertos_template\resources.o: ..\nwatch\resources.h
+01_freertos_template\resources.o: ..\nwatch\typedefs.h
+01_freertos_template\resources.o: C:\Users\jiaweiBai\AppData\Local\Keil_v5\ARM\CompilerV5\Bin\..\include\stdbool.h
+01_freertos_template\resources.o: C:\Users\jiaweiBai\AppData\Local\Keil_v5\ARM\CompilerV5\Bin\..\include\stdint.h
+01_freertos_template\resources.o: ..\nwatch\english.h
